d17ae2c77a2d6fd6d0e587804a2899f195e780f2
Levi Lunique <levi.lunique@gmail.com>
2023-10-16 16:25:25 -0300

    Initial commit - Model Organizacao

---
+++ organizacoes_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Organizacao
+
+admin.site.register(Organizacao)
---
+++ organizacoes_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OrganizacoesAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'organizacoes_app'
